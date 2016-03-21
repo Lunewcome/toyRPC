1d225d8a438258944b0648317ae0bd6ffc006d07
Lunewcome <158390279@qq.com>
2016-03-21 15:06:10 +0800

    This will be a totally asynchronous server.

---
+++ src/async_server.h
@@ -0,0 +1,20 @@
+/**
+ *
+ */
+
+#ifndef SRC_ASYNC_SERVER_H_
+#define SRC_ASYNC_SERVER_H_
+
+#include "common/basics.h"
+
+class AsyncServer {
+ public:
+  AsyncServer() {}
+  ~AsyncServer() {}
+
+ private:
+
+  DO_NOT_COPY_AND_ASSIGN(AsyncServer);
+};
+
+#endif  // SRC_ASYNC_SERVER_H_
